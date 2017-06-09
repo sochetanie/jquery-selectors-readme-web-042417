7c67a28002cf0f1397f983972633bd52295ecd06
sochetanie <allagorik2901@gmail.com>
2017-06-09 10:18:46 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,31 @@
 // declare your functions here...
+// $('div img:first-child')
+
+// $("img[alt='the beatles making faces']")
+
+// $('div:last')
+
+
+function paragraphSelector () {
+    return $('p')
+}
+
+function lastImageSelector () {
+    return $('img:last')
+}
+
+function ninjaBabySelector () {
+    return $('img#baby-ninja')
+}
+
+function divSelector () {
+    return $('.pics')
+}
+
+function firstListItem () {
+    return $('ul#pic-list li:first-child')
+}
+// the same
+// function firstListItem () {
+//     return $('ul > li:first')
+// }
